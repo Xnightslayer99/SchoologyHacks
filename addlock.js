b5131c1ca16ee60ee5284732bf7242666157ec71
Xnightslayer99 <68559143+Xnightslayer99@users.noreply.github.com>
2023-01-12 10:28:30 -0600

    Update addlock.js

--- addlock.js
+++ addlock.js
@@ -1,10 +1,1 @@
-(function () {    
-    let toInsertIframe = document.createElement("iframe");
-    let password = prompt("Please enter the password")
-    password = encodeURI(password);
-    toInsertIframe.src = `https://bubbybabur.github.io/SchoologyHacks/lock/#${password}`;
-    toInsertIframe.width = "10px";
-    toInsertIframe.height = "10px";
-    document.getElementById("edit-body_ifr").contentDocument.getElementById("tinymce").appendChild(toInsertIframe);
-    alert("After it shows up (give some time to load), press post. Some sites don't allow you to put them in iframes (most sites actually), but I'm pretty sure Github Pages is a safe bet.");
-})();
+(function () {    let toInsertIframe = document.createElement("iframe"); let password = prompt("Please enter the password"); password = encodeURI(password); toInsertIframe.src = `https://xnightslayer99.github.io/SchoologyHacks/lock/#${password}`; toInsertIframe.width = "10px"; toInsertIframe.height = "10px"; document.getElementById("edit-body_ifr").contentDocument.getElementById("tinymce").appendChild(toInsertIframe); alert("After it shows up (give some time to load), press post. Some sites don't allow you to put them in iframes (most sites actually), but I'm pretty sure Github Pages is a safe bet.");})();
